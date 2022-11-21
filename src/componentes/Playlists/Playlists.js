2a82d3b2aa7e1ba770271319d12fe3894b33d175
Taynara <montenegro.taynara@gmail.com>
2022-11-21 19:35:16 -0300

    Exercício de Feedback - Integração com API II (async e await) finalizados

--- src/componentes/Playlists/Playlists.js
+++ src/componentes/Playlists/Playlists.js
@@ -4,9 +4,12 @@ import Musicas from "../Musicas/Musicas";
 import { useEffect } from "react";
 
 
+
+
 function Playlists() {
 
     const [playlists, setPlaylists] = useState([])
+    const [pesquisa, setPesquisa] = useState([])
     
     
     const getAllPlaylists = () => {
@@ -27,11 +30,41 @@ function Playlists() {
     }, [])
 
 
+    //Procurar playlists
+    const searchPlaylist = async () => {
+        try {
+            const response = await axios.get(`https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists/search?name=${pesquisa}`, {
+                headers: {
+                    Authorization: "taynara-montenegro-ammal"
+                }
+            })
+
+        console.log(response.data)
+        setPlaylists(response.data.result.playlist);
+        } catch (error) {
+            console.log(error)
+        }
+    };
+
+
+
+    
     return (
         <div>
             {playlists.map((playlist) => {
                 return <Musicas key={playlist.id} playlist={playlist} />
             })}
+
+            
+            <input
+            value={pesquisa}
+            onChange={(e) => setPesquisa(e.target.value)}
+            placeholder="Pesquisar"
+            />
+
+            <button onClick={searchPlaylist}>
+            Pesquisar
+            </button>
 
         </div>
     );
